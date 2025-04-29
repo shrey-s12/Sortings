20083c7be0cc091ab3cfca70c8cb7130eaa459ad
Shrey Singhal <shreynbd12@gmail.com>
2025-04-29 18:01:39 +0530

    Insertion Sort

---
+++ insertionSort.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            if (arr[j] < arr[j - 1])
+            {
+                swap(arr[j], arr[j - 1]);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {8, 2, 4, 1, 5, 7, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
